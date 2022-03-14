2f2b8376ce09d54eba83355011dcbaf0b008bbaa
raphaesl <raphaesl@stud.ntnu.no>
2022-03-14 19:29:39 +0100

    Fix: Added correct initial files.

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file main.cpp
+ * @author Raphael Storm Larsen (raphaesl@stud.ntnu.no)
+ * @brief Main file of Edutrail project.
+ * @version 0.0.0
+ * @date 2022-03-14
+ *  
+ */
+
+#include "LesData2.h";
+#include "Rebus.h";
+#include "Checkpoint.h";
+#include "Team.h";
+#include "miscFunctions.h";
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "Hello World!\n";
+    return 0;
+}
